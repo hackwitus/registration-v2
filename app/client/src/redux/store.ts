9e31dfd3dce55921fca39a582f15fadde633ea39
Roman <rmnlaiacona@gmail.com>
2019-10-14 20:50:14 -0400

    adding redux and redux saga to branch

---
+++ app/client/src/redux/store.ts
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
+
+export default { store };
