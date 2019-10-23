894799936c56c87ed49ca49f8c4d0c4e2a21f99a
Michael Upton <uptonm@wit.edu>
2019-10-23 13:12:26 -0400

    Adding in build

---
+++ app/client/build/precache-manifest.32bbbbb6aa2f3b54e46bec706d87feb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d34a43bc11c6544614a832db7a3ae41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e8c9bc89c817eeca5ff",
+    "url": "/static/css/main.2fc57099.chunk.css"
+  },
+  {
+    "revision": "543a0f285e463b349f86",
+    "url": "/static/js/2.6b28a685.chunk.js"
+  },
+  {
+    "revision": "6e8c9bc89c817eeca5ff",
+    "url": "/static/js/main.3888248f.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "260d57586012b0ed1ae78accc0bf7083",
+    "url": "/static/media/logo.260d5758.png"
+  }
+]);
