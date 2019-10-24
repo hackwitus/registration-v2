03da6c1fca9ed64b5068b67641ef0224fc5fd43a
Mike Upton <uptonm@wit.edu>
2019-10-24 16:51:47 -0400

    did a thing

--- app/client/src/components/ConfirmationCard/index.tsx
+++ app/client/src/components/ConfirmationCard/index.tsx
@@ -1,23 +1,8 @@
 import { Form } from 'semantic-ui-react';
 import * as React from 'react';
-import uuid from 'uuid/v4';
+import { shirt_sizes } from '../../models/Confirmation/initialState';
 
 const ConfirmationCard: React.FC = props => {
-  const shirt_sizes = [
-    { key: uuid(), text: 'Unisex X-Small', value: 'unisex_x_small' },
-    { key: uuid(), text: 'Unisex Small', value: 'unisex_small' },
-    { key: uuid(), text: 'Unisex Medium', value: 'unisex_medium' },
-    { key: uuid(), text: 'Unisex Large', value: 'unisex_large' },
-    { key: uuid(), text: 'Unisex X-Large', value: 'unisex_x_large' },
-    { key: uuid(), text: 'Unisex XX-Large', value: 'unisex_xx_large' },
-    { key: uuid(), text: "Women's X-Small", value: 'womens_x_small' },
-    { key: uuid(), text: "Women's Small", value: 'womens_small' },
-    { key: uuid(), text: "Women's Medium", value: 'womens_medium' },
-    { key: uuid(), text: "Women's Large", value: 'womens_large' },
-    { key: uuid(), text: "Women's X-Large", value: 'womens_x_large' },
-    { key: uuid(), text: "Women's XX-Large", value: 'womens_xx_large' },
-  ];
-
   return (
     <div className="confirmation-card">
       <h1 className="confirmation-card__header">Confirmation</h1>
